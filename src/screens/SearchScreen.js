e6aa32a4c439f3bf816eec6480db1acedaa265df
Daniel Whitney <whitneyd@southwestern.edu>
2021-01-20 13:51:11 -0600

    Updated for react/navigation 5

---
+++ src/screens/SearchScreen.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native'
+
+
+export const SearchScreen = ({ navigation })=>{
+    return <View>
+        <Text>
+            Search Screen
+        </Text>
+    </View>
+}
+
+
+const styles = StyleSheet.create({
+
+})
+
