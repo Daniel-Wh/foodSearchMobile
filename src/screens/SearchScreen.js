315a29c4ad0cd2a810adb6a028efb0534a8aa720
Daniel Whitney <whitneyd@southwestern.edu>
2021-01-22 15:40:25 -0600

    successfully making api calls

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,19 +1,42 @@
 import React, {useState} from 'react';
 import { View, Text, StyleSheet } from 'react-native'
 import {SearchBar} from '../components/SearchBar'
+import yelp from '../api/yelp'
+
 
 
 export const SearchScreen = ({ navigation })=>{
     const [term, setTerm] = useState('')
+    const [errorMessage, setErrorMessage] = useState('')
+    const [results, setResults] = useState([])
+
+    const searchApi = async ()=>{
+        try {
+            const response = await yelp.get('/search', {params: {
+                limit: 50,
+                term,
+                location: 'austin'
+    
+            }})
+            setResults(response.data.businesses)
+
+        } catch (err){
+            setErrorMessage('Something went wrong')
+        }
+        
+    }
+
     return (<View>
         <SearchBar 
         term={term} 
-        onTermChange={newTerm => setTerm(newTerm)}
-        onTermSubmit={(term)=> {console.log(`${term} was submitted`)}}
+        onTermChange={setTerm}
+        onTermSubmit={searchApi}
         />
         <Text>
             Search Screen
         </Text>
+        <Text>We have found {results.length} results</Text>
+        {errorMessage ? <Text>{errorMessage}</Text> : null}
     </View>)
 }
 
